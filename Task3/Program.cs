2500855527f97afcdf12a148045c0ffe68c31b16
Andrpre <andrpre96@gmail.com>
2022-12-01 08:36:41 +0300

    Finish 3 task

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool inCorrect = false;
+
+    while(!inCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            inCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+int numberMy = GetNumber("Введите любое число");
+
+for (int i = 1; i <= numberMy; i++)
+{
+    int numberResult = i * i * i;
+    Console.Write($"{numberResult}, ");
+}
